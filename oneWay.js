5902bbaffd6a8e4065d72724230c88df5e10bf06
rajpaynaik <rajpaynaik18@gmail.com>
2021-11-13 16:22:28 -0500

    first Code

---
+++ oneWay.js
@@ -0,0 +1,29 @@
+const oneWay = (string1, string2) => {
+  let match = [];
+  let string1Split = string1.split("");
+  let string2Split = string2.split("");
+
+  string1Split.some((el) => {
+    if (string2Split.includes(el)) {
+      match.push(el);
+    }
+  });
+
+  console.log(match);
+
+  if (string1Split.length > string2Split.length) {
+    if (string1Split.length - match.length <= 1) {
+      return true;
+    } else {
+      return false;
+    }
+  } else {
+    if (string2Split.length - match.length <= 1) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+};
+
+console.log(oneWay("pale", "bake"));
